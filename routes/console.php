784a90a182fb4a2ddbfe11e972cf15e667181c3a
i-tee <web@tee.su>
2025-08-14 15:26:21 +0300

    The script for automatic debit of funds from the balance has been launched

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+use Illuminate\Support\Facades\Schedule;
+
+
+Schedule::command('vpn:test-charge')->dailyAt('12:23'); // +3h (15:00 MSK)
